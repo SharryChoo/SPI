dde2ce2570e0d8afd25b27a47a67871c7de0a1fc
zhuxiaoyu.sharry <zhuxiaoyu.sharry@bytedance.com>
2021-04-28 21:16:34 +0800

    feature: 修复找不到 api 的问题

--- buildSrc/src/main/kotlin/com/sharry/spi/plugin/core/AnnotationFinder.kt
+++ buildSrc/src/main/kotlin/com/sharry/spi/plugin/core/AnnotationFinder.kt
@@ -1,6 +1,5 @@
 package com.sharry.spi.plugin.core
 
-import com.sharry.spi.plugin.util.Logger
 import org.objectweb.asm.AnnotationVisitor
 import org.objectweb.asm.ClassVisitor
 import org.objectweb.asm.Opcodes
@@ -15,22 +14,22 @@ class AnnotationFinder(api: Int, cv: ClassVisitor?) : ClassVisitor(api, cv) {
         return if (ScanSetting.SERVICE_IMPL_ANNOTATION_DESC == desc) {
             object : AnnotationVisitor(Opcodes.ASM5, super.visitAnnotation(desc, visible)) {
                 override fun visit(name: String, value: Any) {
-                    val entry = AnnotationEntry()
+                    val entry = annotationEntry ?: AnnotationEntry().also {
+                        annotationEntry = it
+                    }
                     when (name) {
                         ScanSetting.SERVICE_IMPL_ANNOTATION_SINGLETON_FILED_NAME -> {
                             entry.singleton = (value as? Boolean) ?: true
                         }
                         ScanSetting.SERVICE_IMPL_ANNOTATION_API_FILED_NAME -> {
-                            entry.serviceApiPath = (value as? Type)?.className ?:""
+                            entry.serviceApiPath = (value as Type).className.replace(".", "/")
                         }
                         ScanSetting.SERVICE_IMPL_ANNOTATION_DELAY_FILED_NAME -> {
                             entry.delay = value as? Boolean ?: true
                         }
                     }
-                    annotationEntry = entry
                     super.visit(name, value)
                 }
-
             }
         } else {
             super.visitAnnotation(desc, visible)
